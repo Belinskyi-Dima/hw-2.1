7fb435a1eb4f6cbc9937a20db0b887b7cae4948a
Dima Belinskyi <84097875+Belinskyi-Dima@users.noreply.github.com>
2021-08-26 15:39:18 +0300

    creat button

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
-'use strict'
-const burgerMenu = document.getElementsByClassName('header__button');
-const burgerMenuList = document.getElementsByClassName('burger__list');
-burgerMenu.addEventListener('click', burgerMenuFn);
-function burgerMenuFn() {
-   burgerMenu.classList.toggle('burger__list');
-   burgerMenuList.classList.toggle('burger__list-active');
-}
+'use strict';
+
+$(document).ready(function(){
+	$('.header__link-buton').on('click', function(){
+		$(this).toggleClass('burger__link-active');
+		$('.burger__list').toggle();
+  });
+});
